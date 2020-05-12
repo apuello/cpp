828f243011440fddb857391844222fa6e56a229e
Ave Puello <avelino.puello@pixelunion.net>
2020-05-11 17:33:16 -0700

    Initial Commit

---
+++ DesignPatterns/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DesignPatterns
+//
+//  Created by Ave Puello on 2020-05-11.
+//  Copyright © 2020 Ave Puello. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
